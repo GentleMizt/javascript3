19f60a03b7449af488f43fcf99c9b93d85b845b0
GentleMizt <olamideiyanda18@gmail.com>
2022-10-17 17:19:14 +0100

    3rd commit

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
         type: 'CBT-BASED',
         duration: '2 hours',
         passmark: 80,
+        display(){
+            console.log(`The Exam i have later today is ${this.subject} and it is a ${this.type} exam. Just so you know the passmark of the exam is ${this.passmark}%`);
+        }
     }
 
     // console.log(test.subject);
@@ -16,7 +19,17 @@
     // console.log(subject);
     // console.log(duration);
     // console.log(`${passmark} %`);
-    let arr = Object.values(test);
-    for (let i = 0; i < arr.length; i++){
-        console.log(Object.keys(test)[i]);
+    
+    let person = {
+        name: 'lancer',
+        age: 20,
+        job: 'Currently hunting'
     }
+
+    let displayObjectProperties = function(myObj){
+        for (let i = 0; i < Object.keys(myObj).length; i++){
+        return Object.keys(myObj)[i];
+        }
+    }
+    displayObjectProperties(person);
+    
