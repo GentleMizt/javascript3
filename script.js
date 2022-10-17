5dedaee29400ca32b1fb98e7424f70beaaaa0a03
GentleMizt <olamideiyanda18@gmail.com>
2022-10-17 16:02:05 +0100

    1st commit

--- script.js
+++ script.js
@@ -4,3 +4,19 @@
         duration: '2 hours',
         passmark: 80,
     }
+
+    // console.log(test.subject);
+    // console.log(test.type);
+    // console.log(test.duration);
+    // console.log(test.passmark);
+
+    // OBJECT DESTRUCTURING!!
+    // let {subject, type, duration, passmark} = test
+    // console.log(type);
+    // console.log(subject);
+    // console.log(duration);
+    // console.log(`${passmark} %`);
+
+    for (let i = 0; i < Object.values(test).length; i++){
+
+    }
